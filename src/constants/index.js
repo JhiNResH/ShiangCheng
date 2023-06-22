7e03b38302adedd4e20565b9b8048d79257b4b00
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-06-23 04:21:38 +0800

    businessbutton

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,21 +23,21 @@ export const features = [
   {
     id: "feature-1",
     icon: star,
-    title: "Rewards",
+    title: "工程",
     content:
       "The best credit cards offer some tantalizing combinations of promotions and prizes",
   },
   {
     id: "feature-2",
     icon: shield,
-    title: "100% Secured",
+    title: "維護、保養",
     content:
       "We take proactive steps make sure your information and transactions are secure.",
   },
   {
     id: "feature-3",
     icon: send,
-    title: "Balance Transfer",
+    title: "買賣、租賃",
     content:
       "A balance transfer credit card can save you a lot of money in interest charges.",
   },
