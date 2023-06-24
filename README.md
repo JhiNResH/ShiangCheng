637572d140da08377b99510f11d43bfb2a85bad7
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-06-24 17:03:37 +0800

    quick

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Shiang-Cheng
-# Shiang-Cheng
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Button = ({ styles }) => {
-  return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient
-      font-poppins font-medium text-[18px] text-primary outline-none`}>
-      聯絡電話
+const Button = ({ styles }) => (
+    <button type='button' className={`py-4 px-6 bg-blue-gradient rounded-[10px]
+      font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      聯絡我們
     </button>
-  )
-}
+  );
 
-export default Button
+export default Button;
